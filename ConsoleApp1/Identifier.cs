bc7ab6fccff719ce83ce01c3178fa73fd7d51423
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-22 18:01:35 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Identifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Identifier
+    {
+        public static string Clean(String frase)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] separar = frase.Split("-");
+            separar[1].ToUpper();
+
+            sb.Append(separar);
+            sb.Replace(" ", "_");
+            sb.Replace("\0", "CTRL");
+            for (int i = 0; i <= 9; i++)
+            {
+                sb.Replace(i.ToString(), ""); 
+            }
+           
+            
+            sb.Replace("-","");
+            sb.ToString().ToUpperInvariant();
+           
+            /*sb.Replace(, "");
+            sb.Replace("[α-ω]", " ");*/
+            return sb.ToString();
+        }
+    }
+}
