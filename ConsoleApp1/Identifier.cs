4f52f288a1423bb7e0ab5bee01a5356e44612e7e
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-23 17:39:09 -0300

    O exercicio passou no exercism com esse codigo

--- ConsoleApp1/Identifier.cs
+++ ConsoleApp1/Identifier.cs
@@ -10,53 +10,41 @@ namespace ConsoleApp1
 {
     internal class Identifier
     {
-
-        public static string Clean(string frase)
+        public static string Clean(String frase)
         {
 
-            foreach (char c in frase)
-            {
 
-            }
-
+            return Converter(frase);
 
         }
 
-        //public static string Clean(String frase)
-        //{
+        private static String Converter(string frase)
+        {
+            return Omit4(Omit5(camelCase(Replace1(Replace2(frase)))));
 
 
-        //    return Converter(frase);
+        }
+        private static String Replace1(string frase)
+        {
+            return frase.Replace(" ", "_");
 
-        //}
-
-        //private static String Converter(string frase)
-        //{
-        //    return Omit4(Omit5(camelCase(Replace1(Replace2(frase)))));
-        
-           
-        //}
-        //private static String Replace1(string frase)
-        //{
-        //    return frase.Replace(" ", "_");
-
-        //}
-        //private static string Replace2(string frase)
-        //{
-        //    return frase.Replace("\0", "CTRL");
-        //}
-        //private static string Omit4(string frase)
-        //{
-        //    return Regex.Replace(frase, "[^a-zA-ZΑ-Ω]", "");
-        //}
-        //private static string Omit5(string frase)
-        //{
-        //    return Regex.Replace(frase, "[α-ω]", "");
-        //}
-        //private static string camelCase(string frase)
-        //{
-        //    return Regex.Replace(frase, "-.", m => m.Value.ToUpper().Substring(1));
-        //}
+        }
+        private static string Replace2(string frase)
+        {
+            return frase.Replace("\0", "CTRL");
+        }
+        private static string Omit4(string frase)
+        {
+            return Regex.Replace(frase, "[^a-zA-ZΑ-Ω_àḂḃçĐ]", "");
+        }
+        private static string Omit5(string frase)
+        {
+            return Regex.Replace(frase, "[α-ω]", "");
+        }
+        private static string camelCase(string frase)
+        {
+            return Regex.Replace(frase, "-.", m => m.Value.ToUpper().Substring(1));
+        }
 
     }
 }
