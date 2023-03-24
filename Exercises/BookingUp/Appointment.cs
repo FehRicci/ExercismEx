153889ed94d2d0f6c481d9fadf9c0f9d7fd81fe9
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-24 18:08:29 -0300

    Codigo refatorado

--- Exercises/BookingUp/Appointment.cs
+++ Exercises/BookingUp/Appointment.cs
@@ -8,36 +8,33 @@ namespace Exercises.BookingUp
 {
     internal class Appointment
     {
-        public Appointment()
-        {
-
-        }
         public static DateTime Schedule(string informedDate)
         {
-            string[] parts = informedDate.Split('/', ':', ' ');
-            int year = int.Parse(parts[2]);
-            int month = int.Parse(parts[0]);
-            int day = int.Parse(parts[1]);
-            int hour = int.Parse(parts[3]);
-            int minute = int.Parse(parts[4]);
-            int second = int.Parse(parts[5]);
-
-            DateTime date = new DateTime(year, month, day, hour, minute, second);
+            DateTime date = DateTime.Parse(informedDate);
 
             return date;
         }
 
         public static bool HasPassed(DateTime date)
         {
-            if (date<=DateTime.Now) return true ;
-            else return false ;
-
+            if (date <= DateTime.Now) return true;
+            else return false;
         }
 
         public static bool IsAfternoonAppointment(DateTime date)
         {
-            if (date.Hour>=12 && date.Hour<18) return true;
-            else return false ;
+            if (date.Hour >= 12 && date.Hour < 18) return true;
+            else return false;
+        }
+
+        public static string Description(DateTime date)
+        {
+            return "You have an appointment on " + date + ".";
+        }
+
+        public static DateTime AnniversaryDate()
+        {
+            return new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
         }
     }
 }
