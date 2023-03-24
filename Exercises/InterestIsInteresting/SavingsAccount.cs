baa1abd8d16cbed00ebdef864e6aae3c5bef289a
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-24 19:52:11 -0300

    Feitos os passos 1 e 2 do exercicio

--- Exercises/InterestIsInteresting/SavingsAccount.cs
+++ Exercises/InterestIsInteresting/SavingsAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,36 @@ namespace Exercises.InterestIsInteresting
 {
     internal class SavingsAccount
     {
+        //3,213% para saldo negativo(saldo fica mais negativo).
+        //0,5% para um saldo positivo inferior a 1000dólares.
+        //1,621% para saldo positivo maior ou igual a 1000dólares e menor que 5000dólares.
+        //2,475% para saldo positivo maior ou igual a 5000dólares.
+
+        public static float InterestRate(decimal balance)
+        {
+            if (balance < 0)
+                return 3.213f;
+            else if (balance < 1000)
+                return 0.5f;
+            else if (balance >= 1000 && balance < 5000)
+                return 1.621f;
+            else if (balance >= 5000)
+                return 2.475f;
+            else 
+                return 0.0f;
+        }
+
+        public static decimal Interest(decimal balance)
+        {
+            
+            decimal interest = Convert.ToDecimal(InterestRate(balance));
+
+            return interest * balance;
+
+
+        }
+
+
 
     }
 }
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Exercises.BookingUp;
+using Exercises.InterestIsInteresting;
 using Exercises.SqueakyClean;
 
 internal class Program
@@ -13,15 +14,17 @@ internal class Program
         //Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
         //Console.WriteLine(Identifier.Clean("a-bc"));
 
-        Console.WriteLine(Appointment.Schedule("Thursday, July 25, 2019 13:45:00"));
-        Console.WriteLine(Appointment.Schedule("July 25, 2019 13:45:00"));
-        Console.WriteLine(Appointment.Schedule("7/25/2019 13:45:00"));
-        Console.WriteLine(Appointment.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0)));
-        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0)));
-        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 10, 0, 0)));
-        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 19, 0, 0)));
-        Console.WriteLine(Appointment.Description(new DateTime(2019, 03, 29, 15, 0, 0)));
-        Console.WriteLine(Appointment.AnniversaryDate());
+        //Console.WriteLine(Appointment.Schedule("Thursday, July 25, 2019 13:45:00"));
+        //Console.WriteLine(Appointment.Schedule("July 25, 2019 13:45:00"));
+        //Console.WriteLine(Appointment.Schedule("7/25/2019 13:45:00"));
+        //Console.WriteLine(Appointment.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0)));
+        //Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0)));
+        //Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 10, 0, 0)));
+        //Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 19, 0, 0)));
+        //Console.WriteLine(Appointment.Description(new DateTime(2019, 03, 29, 15, 0, 0)));
+        //Console.WriteLine(Appointment.AnniversaryDate());
 
+        Console.WriteLine(SavingsAccount.InterestRate(balance: 200.75m));
+        Console.WriteLine(SavingsAccount.Interest(balance: 200.75m));
     }
 }
