28eaafd86cd34aa00131085c23d8cc46d40d5f72
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-24 18:14:48 -0300

    Exercicio de DateTime Booking up for Beauty (#2)

---
+++ Exercises/BookingUp/Appointment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.BookingUp
+{
+    internal class Appointment
+    {
+        public static DateTime Schedule(string informedDate)
+        {
+            DateTime date = DateTime.Parse(informedDate);
+
+            return date;
+        }
+
+        public static bool HasPassed(DateTime date)
+        {
+            if (date <= DateTime.Now) return true;
+            else return false;
+        }
+
+        public static bool IsAfternoonAppointment(DateTime date)
+        {
+            if (date.Hour >= 12 && date.Hour < 18) return true;
+            else return false;
+        }
+
+        public static string Description(DateTime date)
+        {
+            return "You have an appointment on " + date + ".";
+        }
+
+        public static DateTime AnniversaryDate()
+        {
+            return new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,17 +1,28 @@
-﻿using Exercises.SqueakyClean;
+
+using Exercises.BookingUp;
+using Exercises.SqueakyClean;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
 
-        Console.WriteLine(Identifier.Clean("my   Id"));
-        Console.WriteLine(Identifier.Clean("my\0Id"));
-        Console.WriteLine(Identifier.Clean("à-ḃç"));
-        Console.WriteLine(Identifier.Clean("1😀2😀3😀"));
-        Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
-        Console.WriteLine(Identifier.Clean("a-bc"));
+        //Console.WriteLine(Identifier.Clean("my   Id"));
+        //Console.WriteLine(Identifier.Clean("my\0Id"));
+        //Console.WriteLine(Identifier.Clean("à-ḃç"));
+        //Console.WriteLine(Identifier.Clean("1😀2😀3😀"));
+        //Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
+        //Console.WriteLine(Identifier.Clean("a-bc"));
 
+        Console.WriteLine(Appointment.Schedule("Thursday, July 25, 2019 13:45:00"));
+        Console.WriteLine(Appointment.Schedule("July 25, 2019 13:45:00"));
+        Console.WriteLine(Appointment.Schedule("7/25/2019 13:45:00"));
+        Console.WriteLine(Appointment.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0)));
+        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0)));
+        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 10, 0, 0)));
+        Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 19, 0, 0)));
+        Console.WriteLine(Appointment.Description(new DateTime(2019, 03, 29, 15, 0, 0)));
+        Console.WriteLine(Appointment.AnniversaryDate());
 
     }
 }
