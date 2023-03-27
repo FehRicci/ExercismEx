945b9ad10866f8b3700e329a7671f3941342cd0d
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-27 15:13:27 -0300

    Criando nova classe e refazendo o programa, a forma que havia feito não foi aceita no exercism

---
+++ Exercises/Triangle/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Triangle
+{
+    internal class Triangle
+    {
+        //public static bool IsScalene(double side1, double side2, double side3)
+        //{
+        //    throw new NotImplementedException("You need to implement this function.");
+        //}
+
+        //public static bool IsIsosceles(double side1, double side2, double side3)
+        //{
+        //    throw new NotImplementedException("You need to implement this function.");
+        //}
+
+        //public static bool IsEquilateral(double side1, double side2, double side3)
+        //{
+        //    throw new NotImplementedException("You need to implement this function.");
+        //}
+    }
+}
