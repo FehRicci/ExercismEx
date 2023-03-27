ddf570c3edb4785a18e0b664ebb8952a9969045c
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-27 15:21:54 -0300

    Feitos os verdadeiro ou falso de cada tipo de triangulo

--- Exercises/Triangle/Triangle.cs
+++ Exercises/Triangle/Triangle.cs
@@ -8,19 +8,29 @@ namespace Exercises.Triangle
 {
     internal class Triangle
     {
-        //public static bool IsScalene(double side1, double side2, double side3)
-        //{
-        //    throw new NotImplementedException("You need to implement this function.");
-        //}
 
-        //public static bool IsIsosceles(double side1, double side2, double side3)
-        //{
-        //    throw new NotImplementedException("You need to implement this function.");
-        //}
+        public static bool IsScalene(double side1, double side2, double side3)
+        {
+            if (side1 != side2 && side2 != side3 && side1 != side3)
+                return true; 
+            else 
+                return false;
+        }
 
-        //public static bool IsEquilateral(double side1, double side2, double side3)
-        //{
-        //    throw new NotImplementedException("You need to implement this function.");
-        //}
+        public static bool IsIsosceles(double side1, double side2, double side3)
+        {
+            if (side1 == side2 || side1 == side3 || side2 == side3) 
+                return true;
+            else 
+                return false;
+        }
+
+        public static bool IsEquilateral(double side1, double side2, double side3)
+        {
+            if (side1 == side2 && side2 == side3) 
+                return true;
+            else
+                return false;
+        }
     }
 }
