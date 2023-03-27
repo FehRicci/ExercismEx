29e4d43e0040e47d08ae5de6dee581b863a9fe6a
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-27 17:09:45 -0300

    Programa para inverter string

--- Exercises/ProteinTranslation/ProteinTranslation.cs
+++ Exercises/ProteinTranslation/ProteinTranslation.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Exercises.ProteinTranslation
-{
-    internal class ProteinTranslation
-    {
-    }
-}
+//namespace Exercises.ProteinTranslation
+//{
+//    internal class ProteinTranslation
+//    {
+//        //Codon               Protein
+//        //AUG                 Methionine
+//        //UUU, UUC            Phenylalanine
+//        //UUA, UUG            Leucine
+//        //UCU, UCC, UCA, UCG  Serine
+//        //UAU, UAC            Tyrosine
+//        //UGU, UGC            Cysteine
+//        //UGG                 Tryptophan
+//        //UAA, UAG, UGA       STOP
+
+//        public static string[] Proteins(string strand)
+//        {
+//            strand = strand.Trim().ToUpper();
+//            string stop = "UAA";
+//            string[] test = strand.Split();
+//            int endIndex = strand.LastIndexOf(stop);
+//        }
+
+//        private static string Stop()
+//        {
+//            string stop1 = "UAA";
+//            string stop2 = "UAG";
+//            string stop3 = "UGA";
+//        }
+//    }
+//}
