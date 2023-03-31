a706ae542471e30b57ba2964dc13d5975157832c
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-31 17:58:01 -0300

    Iniciando codigo para exercicio Binary Search

---
+++ Exercises/BinarySearch/BinarySearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.BinarySearch
+{
+    internal class BinarySearch
+    {
+        public static int Find(int[] input, int value)
+        {
+           
+        }
+    }
+}
