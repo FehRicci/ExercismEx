290c11265d3172788f725ddf00bd206964a927b6
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-31 16:04:32 -0300

    Codigo passou em todos os testes

--- Exercises/Leap/Leap.cs
+++ Exercises/Leap/Leap.cs
@@ -10,7 +10,10 @@ namespace Exercises.Leap
     {
         public static bool IsLeapYear(int year)
         {
-            
+            if ((year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0))
+            return true;
+            else
+            return false;
         }
     }
 }
