909c3d0a109bbab65303e073577a29694f7981db
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-03-31 15:44:20 -0300

    Iniciando programa Leap Year

---
+++ Exercises/Leap/Leap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Leap
+{
+    internal class Leap
+    {
+        public static bool IsLeapYear(int year)
+        {
+            
+        }
+    }
+}
