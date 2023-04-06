7a51fe9af3b15ca91e621eac07a6d7e450deab79
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-06 18:14:54 -0300

    Exercise Need for speed from Exercism (#13)

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -9,11 +9,24 @@ using Exercises.Pangram;
 using Exercises.ReverseString;
 using Exercises.SqueakyClean;
 using Exercises.Triangle;
+using Exercises.NeedForSpeed;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+
+        int speed = 100;
+        int batteryDrain = 60;
+        var car = new RemoteControlCar(speed, batteryDrain);
+        int distance = 16;
+        var race = new RaceTrack(distance);
+        car.Drive();
+        car.Drive();
+        Console.WriteLine(car.BatteryDrained());
+        Console.WriteLine(car.DistanceDriven());
+        Console.WriteLine(race.TryFinishTrack(car));
+
 
     }
 }
