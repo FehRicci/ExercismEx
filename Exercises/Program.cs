3553d4b298442ac7ee72b2a2d21f26f6d46a95c6
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-06 17:45:08 -0300

    Exercise Space age from exercism (#14)
    * Iniciando exercicio Space Age do Exercism
    
    * Implementado o codigo do exercício SpaceAge
    
    * Codigo refatorado seguindo feedback
    
    * Codigo refatorado

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -15,56 +15,5 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        //Console.WriteLine(Identifier.Clean("my   Id"));
-        //Console.WriteLine(Identifier.Clean("my\0Id"));
-        //Console.WriteLine(Identifier.Clean("à-ḃç"));
-        //Console.WriteLine(Identifier.Clean("1😀2😀3😀"));
-        //Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
-        //Console.WriteLine(Identifier.Clean("a-bc"));
-
-        //Console.WriteLine(Appointment.Schedule("Thursday, July 25, 2019 13:45:00"));
-        //Console.WriteLine(Appointment.Schedule("July 25, 2019 13:45:00"));
-        //Console.WriteLine(Appointment.Schedule("7/25/2019 13:45:00"));
-        //Console.WriteLine(Appointment.HasPassed(new DateTime(1999, 12, 31, 9, 0, 0)));
-        //Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 15, 0, 0)));
-        //Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 10, 0, 0)));
-        //Console.WriteLine(Appointment.IsAfternoonAppointment(new DateTime(2019, 03, 29, 19, 0, 0)));
-        //Console.WriteLine(Appointment.Description(new DateTime(2019, 03, 29, 15, 0, 0)));
-        //Console.WriteLine(Appointment.AnniversaryDate());
-
-        //Console.WriteLine(SavingsAccount.InterestRate(balance: 200.75m));
-        //Console.WriteLine(SavingsAccount.Interest(balance: 200.75m));
-        //Console.WriteLine(SavingsAccount.AnnualBalanceUpdate(balance: 200.75m));
-        //Console.WriteLine(SavingsAccount.YearsBeforeDesiredBalance(balance: 200.75m, targetBalance: 214.88m));
-
-        //Console.WriteLine(Pangram.checkPangram("OI"));
-        //Console.WriteLine(Pangram.checkPangram("The quick brown fox jumps over the lazy dog"));
-
-        //Console.WriteLine("Equelatero :" + Triangle.IsEquilateral(5,5,5));
-        //Console.WriteLine("Equelatero :" + Triangle.IsEquilateral(5, 10, 5));
-        //Console.WriteLine("Isosceles :" + Triangle.IsIsosceles(5, 5, 5));
-        //Console.WriteLine("Isosceles :" + Triangle.IsIsosceles(5, 10, 5));
-        //Console.WriteLine("Escaleno :" + Triangle.IsScalene(5, 5, 5));
-        //Console.WriteLine("Escaleno :" + Triangle.IsScalene(1, 2, 3));
-        //Console.WriteLine("Isosceles :" + Triangle.IsIsosceles(1, 1, 3));
-
-        //Console.WriteLine(ReverseString.Reverse("Fernanda"));
-
-        //Console.WriteLine(Isogram.IsIsogram("Fernanda"));
-        //Console.WriteLine(Isogram.IsIsogram("lumberjacks"));
-        //Console.WriteLine(Isogram.IsIsogram("background"));
-        //Console.WriteLine(Isogram.IsIsogram("Alphabet"));
-
-        //Console.WriteLine(ProteinTranslation.Proteins("AUGUUUUCU"));
-
-        //Console.WriteLine(Leap.IsLeapYear(1997));
-        //Console.WriteLine(Leap.IsLeapYear(1996));
-        //Console.WriteLine(Leap.IsLeapYear(1900));
-        //Console.WriteLine(Leap.IsLeapYear(2000));
-
-        //Console.WriteLine(Darts.Score(-9, 9));
-        //Console.WriteLine(Darts.Score(-0.1, -0.1));
-        //Console.WriteLine(Darts.Score(-3.5, 3.5));
-
     }
 }
---
+++ Exercises/SpaceAge/SpaceAge.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.SpaceAge
+{
+    internal class SpaceAge
+    {
+        private float _informedYear;
+
+        public SpaceAge(int seconds)
+        {
+            const float yearInSeconds = 31557600f;
+            _informedYear = seconds / yearInSeconds;
+        }
+
+        public float OnEarth() => _informedYear;
+
+        public float OnMercury() => _informedYear / orbitalPeriodInYears("Mercury");
+
+        public float OnVenus() => _informedYear / orbitalPeriodInYears("Venus");
+
+        public float OnMars() => _informedYear / orbitalPeriodInYears("Mars");
+
+        public float OnJupiter() => _informedYear / orbitalPeriodInYears("Jupiter");
+
+        public float OnSaturn() => _informedYear / orbitalPeriodInYears("Saturn");
+
+        public float OnUranus() => _informedYear / orbitalPeriodInYears("Uranus");
+
+        public float OnNeptune() => _informedYear / orbitalPeriodInYears("Neptune");
+
+        //Mercury: orbital period 0.2408467 Earth years
+        //Venus: orbital period 0.61519726 Earth years
+        //Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+        //Mars: orbital period 1.8808158 Earth years
+        //Jupiter: orbital period 11.862615 Earth years
+        //Saturn: orbital period 29.447498 Earth years
+        //Uranus: orbital period 84.016846 Earth years
+        //Neptune: orbital period 164.79132 Earth years
+
+        private float orbitalPeriodInYears(string planet)
+        {
+            switch (planet)
+            {
+                case "Mercury":
+                    return 0.2408467f;
+                case "Venus":
+                    return 0.61519726f;
+                case "Mars":
+                    return 1.8808158f;
+                case "Jupiter":
+                    return 11.862615f;
+                case "Saturn":
+                    return 29.447498f;
+                case "Uranus":
+                    return 84.016846f;
+                case "Neptune":
+                    return 164.79132f;
+                default:
+                    return 1f;
+            }
+        }
+    }
+}
+
