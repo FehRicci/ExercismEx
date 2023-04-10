10ef869c1443737d3d7d88c7b7ffe97de6132570
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-10 17:42:19 -0300

    Codigo passou em todos os testes

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -15,8 +15,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
-        var value = 11;
-        Console.WriteLine(BinarySearch.Find(array, value));
+
     }
 }
---
+++ Exercises/ResistorColor/ResistorColor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.ResistorColor
+{
+    internal class ResistorColor
+    {
+        public static int ColorCode(string color)
+        {
+            switch (color.ToLower())
+            {
+                case "black":
+                    return 0;
+                case "brown":
+                    return 1;
+                case "red":
+                    return 2;
+                case "orange":
+                    return 3;
+                case "yellow":
+                    return 4;
+                case "green":
+                    return 5;
+                case "blue":
+                    return 6;
+                case "violet":
+                    return 7;
+                case "grey":
+                    return 8;
+                case "white":
+                    return 9;
+                default:
+                    return 10;
+            }
+            
+        }
+
+        public static string[] Colors()
+        {
+           return new string[10]{"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+        }
+    }
+}
