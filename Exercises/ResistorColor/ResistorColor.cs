e314c5b8d9207eed426241ccfaf08845be911d38
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-11 18:01:55 -0300

    Feita refatoração apos perceber algumas possiveis modificações

--- Exercises/ResistorColor/ResistorColor.cs
+++ Exercises/ResistorColor/ResistorColor.cs
@@ -10,19 +10,15 @@ namespace Exercises.ResistorColor
     {
         public static int ColorCode(string color)
         {
-             int i = 0;
-            for (i = 0; i < Colors().Length; i++)
+            for (int i = 0; i < Colors().Length; i++)
             {
                 if (Colors()[i] == color.ToLower())
                     return i;
             }
             return -1;
-          
         }
 
-        public static string[] Colors()
-        {
-           return new string[10]{"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
-        }
+        public static string[] Colors() => new string[]{"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+
     }
 }
