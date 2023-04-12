f017293a68d4d3570d18fe49996d4984fadf17cd
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-12 15:33:45 -0300

    Exercise Gigasecond from Exercism (#16)

---
+++ Exercises/Gigasecond/Gigasecond.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Gigasecond
+{
+    internal static class Gigasecond
+    {
+        private static float _gigasecond = 1e9f;
+
+        public static DateTime Add(DateTime moment) => moment.AddSeconds(_gigasecond);
+    }
+}
