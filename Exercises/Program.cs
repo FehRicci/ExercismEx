657739024ca155c89acef0713cd41678dd3b6d78
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-12 17:34:42 -0300

    Exercise Sum of multiples from Exercism (#15)

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -10,12 +10,13 @@ using Exercises.ReverseString;
 using Exercises.SqueakyClean;
 using Exercises.Triangle;
 using Exercises.NeedForSpeed;
+using Exercises.SumOfMultiples;
 using Exercises.Gigasecond;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine(Gigasecond.Add(new DateTime(1993,04,29,20,58,00)));
+        Console.WriteLine(SumOfMultiples.Sum(new[] { 3, 5 }, 20));
     }
 }
---
+++ Exercises/SumOfMultiples/SumOfMultiples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.SumOfMultiples
+{
+    internal static class SumOfMultiples
+    {
+        public static int Sum(IEnumerable<int> multiplesBase, int upperIndex)
+        {
+            int sum = 0;
+
+            foreach (int value in multiples(multiplesBase, upperIndex))
+            {
+                sum += value;
+            }
+            return sum;
+        }
+
+        private static List<int> multiples(IEnumerable<int> multiplesBase, int upperIndex)
+        {
+            List<int> multiple = new();
+
+            foreach (int value in multiplesBase)
+            {
+                if (value != 0)
+                {
+                    for (int i = value; i < upperIndex; i = i + value)
+                    {
+                            multiple.Add(i);
+                    }
+                }
+            }
+            return multiple.Distinct().ToList();
+        }
+    }
+}
