3c7a7ad697db70b060818c020684c893f029c57f
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-04-18 14:15:01 -0300

    Exercise Difference of Squares from Exercism  (#18)

---
+++ Exercises/DifferenceOfSquares/DifferenceOfSquares.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.DifferenceOfSquares
+{
+    internal class DifferenceOfSquares
+    {
+        public static double CalculateDifferenceOfSquares(int value)
+        {
+            return Math.Abs(CalculateSumOfSquares(value) - CalculateSquareOfSum(value));
+        }
+
+        public static double CalculateSquareOfSum(int value)
+        {
+            int sum = 0;
+            for (int i = 1; i <= value; i++)
+            {
+                sum += i;
+            }
+            return Math.Pow(sum,2);
+        }
+
+        public static double CalculateSumOfSquares(int value)
+        {
+            double sum = 0;
+            for (int i = 1; i <= value; i++)
+            {
+                sum = Math.Pow(i,2) + sum;
+            }
+            return sum;
+        }
+    }
+}
